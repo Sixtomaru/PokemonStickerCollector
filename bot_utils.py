48ba745fd10dc3833be1c81f0df8c0029a366fbd
Sixtomaru <sixtomaru@gmail.com>
2026-02-19 03:15:53 +0100

    mini stickers añadidos

--- bot_utils.py
+++ bot_utils.py
@@ -12,3 +12,11 @@ def get_rarity(category, is_shiny):
     """Calcula la rareza final de un Pokémon."""
     if not is_shiny: return category
     return 'S' if category in ['C', 'B'] else 'SS' if category == 'A' else 'SSS'
+
+def get_formatted_name(pokemon_data, is_shiny=False):
+    """Devuelve el nombre formateado con Emoji + Nombre + Estrella (si es shiny)."""
+    # Si tenemos emoji personalizado, lo usamos.
+    emoji = f"<tg-emoji emoji-id='{pokemon_data['emoji_id']}'>⬜</tg-emoji>" if 'emoji_id' in pokemon_data else ""
+
+    shiny_text = " ✨" if is_shiny else ""
+    return f"{emoji} {pokemon_data['name']}{shiny_text}"
