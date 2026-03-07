5f63db3beb31c8a321359a8a38ad2c9a77685812
Sixtomaru <sixtomaru@gmail.com>
2026-03-07 23:52:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5392,7 +5392,7 @@ def main():
         # Creamos la lista de botones URL (cada uno en su propia fila)
         keyboard = [
             [InlineKeyboardButton("🗓 Calendario de Eventos Especiales", url="https://t.me/pokemon_shuffle/136468")],
-            [InlineKeyboardButton("👥 Lista de Códigos Shuffle", url="https://t.me/pokemon_shuffle/376643")],
+            [InlineKeyboardButton("👥 Lista de Códigos Shuffle", url="https://t.me/pokemon_shuffle/376662")],
             [InlineKeyboardButton("🏆 Retos de Grupo", url="https://t.me/pokemon_shuffle/172831")],
             [InlineKeyboardButton("🗣 Off-Topic", url="https://t.me/joinchat/BJ0pDg7ntNfIFuVonHSTiQ")],
             [InlineKeyboardButton("🔴 Pokémon Go", url="https://t.me/joinchat/Y4wR9kZnCX0zZTVk")],
@@ -5522,3 +5522,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
